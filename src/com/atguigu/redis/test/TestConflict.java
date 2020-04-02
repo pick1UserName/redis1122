12974e9aee9e2b791026d726eecc09481ce5eeb4
shitou <shitou.atguigu.com>
2020-04-02 10:41:53 +0800

    test conflict right

---
+++ src/com/atguigu/redis/test/TestConflict.java
@@ -0,0 +1,14 @@
+package com.atguigu.redis.test;
+/**
+*
+*@author:jt   
+*@date:2020年4月2日  上午10:39:31
+*/
+
+public class TestConflict {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("turn right");
+	}
+}
